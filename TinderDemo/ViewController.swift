7c0e52894b44a98f0412f2897637ffebb6686328
Andy Guinto <andymatthew.guinto@gmail.com>
2019-01-08 20:26:34 -0500

    Initial Commit

---
+++ TinderDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TinderDemo
+//
+//  Created by Andy Guinto on 1/8/19.
+//  Copyright © 2019 Andy Guinto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
